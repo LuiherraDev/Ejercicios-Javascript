1a3bcb08e171eb38c97db25023f6369afcc6f8dd
luis <luiherra25@gmail.com>
2025-11-18 17:42:19 +0100

    Primeros Ejercicios con Booleanos y condicionales

--- script.js
+++ script.js
@@ -297,4 +297,132 @@ console.log(grupo.integrantes.length)
 // Documentación que puedes buscar para resolverlos: Objetos, como acceder a objetos, como modificar objetos 
 
 
+/* ------ Lógica booleana ------ */
+// Dados 2 variables booleanas:
+const bool1=true;
+const bool2=true
+console.log(bool1,bool2)
+
+// Crear variable booleanoAnd cuyo valor sea la comparación booleana booleano1 and booleano2
+const booleanoAnd=bool1 && bool2
+console.log(booleanoAnd)
+
+const booleanoAnd2=bool1 && bool2 && "Hola"
+console.log("!!!!!!!",booleanoAnd2)
+
+// Crear variable booleanoOr cuyo valor sea la comparación booleana booleano1 or booleano2
+const booleanoOr=bool1 || bool2
+console.log(booleanoOr)
+
+// Crear variable booleanoNot cuyo valor sea la comparación booleana no booleano1
+const booleanoNot=!bool1
+console.log(booleanoNot)
+
+// Crear variable booleanoMix0 cuyo valor sea la comparación booleana (booleano1 or booleano2) and (booleano1 or (not booleano1 and not booleano2)
+const booleanoMix0=(bool1||bool2)&&(bool1||(!bool1&&!bool2))
+console.log(booleanoMix0)
+
+console.log("Valor de Boolean",Boolean(!!"Hola"),Boolean(undefined),Boolean([]),Boolean({}))
+
+// -----------------------------------
+// Crear variable valorDivisión cuyo valor sea la división de 17 entre 3 y que solo muestre 2 decimales en el resultado
+const valorDivision=Math.trunc((17/3)*100)/100
+console.log(valorDivision)
+
+// Crear variable valorResto cuyo valor sea el resto de 17 entre 7
+const valorResto=17%7
+console.log(valorResto)
+
+
+// ----------------PRUEBA DE BUCLES-------------------
+const listaCompra=["manzana","macarrones",45,true,"patata"]
+
+for(let i=0;i<listaCompra.length;i++){
+    console.log(listaCompra[i])
+}
+
+for (const key in listaCompra) {    
+    console.log(listaCompra[key],key)
+}
+
+for (const element of listaCompra) {
+    console.log(element)
+}
+
+listaCompra.forEach((element,index) => {
+    console.log("ForEach",element,index)
+});
+
+
+// Crea una variable A cuyo valor sea el número 9, después crea una variable B cuyo valor sea un string con el número 9, desarrolla la lógica para que cuando A == B muestre por consola “Son iguales”, ¿qué pasa si en vez de tener A == B, comprobamos que A === B?
+const A=9;
+const B="9";
+if(A===B){
+    console.log("Son iguales")
+} else {
+    console.log("No son iguales o no es el mismo tipo de dato")
+}
+
+
+// Crea una variable mochila que sea un array de elementos, desarrolla la lógica para que sí mochila tiene más de 10 elementos muestre por consola el mensaje “No puedo cargar con tantas cosas”, sí mochila contiene entre 10 y 2 elementos se muestre por consola “Que bien voy con mi mochila” y si no muestre por consola “Creo q no necesito una mochila”
+const mochila=[1,2]
+if(mochila.length > 10){
+    console.log("No puedo cargar con tantas cosas")
+} else if (mochila.length >= 2 && mochila.length<=10){
+    console.log("Que bien voy con mi mochila")
+} else {
+    console.log("Creo q no necesito una mochila")
+}
+
+// Crear variable contarHasta10 con valor 0 e incrementar su valor con un bucle hasta que se verifique que contarHasta10 === 10
+let contarHasta10=0;
+for(let i=0;i<10;i++){
+    // console.log(i)
+    contarHasta10+=1
+    // contadorHasta10 = contadorHasta10 + 1
+    // console.log(valorNuevo)
+    if(contarHasta10===10){
+        console.log("contarHasta10 vale: ",contarHasta10)
+    }
+}
+console.log(contarHasta10)
+
+
+const array=["Hola","como","estas"]
+let string=""
+for(let i=0;i<array.length;i++){
+  console.log(i)
+  console.log(array[i])
+  string = string  + array[i] + " "
+}
+
+// Crea una variable diaFestivo que sea de tipo boolean. Crea un ternario que si diaFestivo es true devuelve un console.log de ‘Hoy no trabajo’ y  si es false ‘Hoy trabajo’
+
+
+// Crea una variable arrayBucle tipo array vacía, rellénala con números del 4 al 18 utilizando un bucle
+
+
+// Recorre la variable creada anteriormente arrayBucle ,suma todos sus elementos y guárdalos en una variable de nombre resultado
+
+
+// Dado el siguiente array, [‘Con’, ‘Sofia’, ‘aprendiendo’, ‘bucles’], recorrelo utilizando el bucle “for of” y muestra por consola todos sus elementos, después recórrelo utilizando el bucle “for in” mostrando también por consola todos sus elementos
+
+
+// Crea un bucle utilizando “while” que itere 20 veces y muestre por consola “Patata” cada vez que el número de la iteración sea múltiplo de 3
+
+
+// Crea un programa el cual te pregunte por una nota del 0 al 10 y dependiendo del número, te devuelva la siguiente clasificación.
+// Nota: Debes de usar el Switch.
+// 0 - 4: Insuficiente.
+// 5 - 6: Suficiente.
+// 7 - 8: Notable.
+// 9 - 10: Sobresaliente.
+
+
+// Crea una función resta que espere dos parámetros a y b y que devuelva la resta de los mismos.
+
+
+// Crea la función duplicaNumero debe recibir un tipo number y devolver el doble del valor recibido. Si la función no recibe un dato tipo number debe devolver el string ‘Debo ser ejecutada con un número’
+
+
 
